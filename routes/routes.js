e69c65f1a894ffa719462b228e4fe331d3094e19
Mims70 <anyiammiracle7777@gmail.com>
2024-03-24 22:23:26 +0100

    Added Node 2 task

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 // Import middleware
-const authenticateToken = require('../middleware/authenticateToken');
+const authenticateToken = require('../middleware/authenticatetoken');
 const authorizeRoles = require('../middleware/authorizeroles');
 const validateData = require('../middleware/validatedata');
 
